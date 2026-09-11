e611db4ba9de2b067c0f02f39fd87c5b68ed55d0
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-09-11 16:15:24 +0000

    tables creation

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 from dotenv import load_dotenv
 
@@ -10,6 +10,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = DeclarativeBase()
 
 def get_db():
     db = SessionLocal()
--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from models import User
-
+from database import get_db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.get("/register")
-def register(details:User):
-    return{details}
+def register(details:User, db: Session = Depends(get_db)):
+    pass
     

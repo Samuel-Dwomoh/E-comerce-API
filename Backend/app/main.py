2b748708407c4233e80b104c33c61a24ad165273
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-09-14 10:23:13 +0000

    CORS implemented

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routers import auth, cart, categories, orders, product, reviews, users
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +15,15 @@ app.include_router(product.router)
 app.include_router(reviews.router)
 app.include_router(users.router)
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["home"])
 def home():

595f2b29cb48e09f6c364f31ec82c37235ad5d09
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-09-15 19:54:33 +0000

    get review implemented

--- Backend/app/routers/reviews.py
+++ Backend/app/routers/reviews.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from schemas.schemas import Reviews
 from database import get_db
 router = APIRouter(prefix="/reviews", tags=["reviews"])
 
 @router.get("/reviews/{id}/reviews")
 def get_reviews(id:int, db:Session = Depends(get_db)):
-    get_review = db.query(Reviews).filter(Reviews)
+    get_review = db.query(Reviews).filter(Reviews.product_id == id).all()
+    return get_review

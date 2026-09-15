2c9424b42c5c742773c00530c1dc05feaa489521
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-09-15 19:42:38 +0000

    review endpoint

--- Backend/app/routers/reviews.py
+++ Backend/app/routers/reviews.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
-
+from sqlalchemy.orm import Session
+from database import get_db
 router = APIRouter(prefix="/reviews", tags=["reviews"])
+
+@router.get("/reviews")
+def get_reviews():
+    pass

596c06f1fa240b13770ffe5cd5bcd5a6835f3094
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-09-17 20:42:40 +0000

    get_user implemented

--- Backend/app/routers/users.py
+++ Backend/app/routers/users.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, Depends
 from database import get_db
 from sqlalchemy.orm import Session
+from schemas.schemas import Users
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/users/{id}")
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(Users).filter(Users.id == id).first()
+    if not user:
+        return{"message":"User not found"}
+    return user

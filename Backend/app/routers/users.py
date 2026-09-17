ea328f0e02799d59ca435ac9be7c0d26a95ac370
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-09-17 20:38:42 +0000

    users endpoint implementation

--- Backend/app/routers/users.py
+++ Backend/app/routers/users.py
@@ -1,3 +1,6 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from database import get_db
+from sqlalchemy.orm import Session
 router = APIRouter(prefix="/users", tags=["users"])
+
+@router.get("/users/{id}")
